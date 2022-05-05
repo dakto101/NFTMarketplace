8fe993ba6758a45870b3ad05b8b41b951ebc405e
hcraft <36808098+dakto101@users.noreply.github.com>
2022-05-05 19:29:29 +0700

    05052022 update
    Detail NFT update.

--- pages/detail-nft.js
+++ pages/detail-nft.js
@@ -25,7 +25,7 @@ export default function DetailNFT() {
     
   }, [id])
 
-
+/*
   async function loadNFTs() {
     const web3Modal = new Web3Modal({
       network: "mainnet",
@@ -51,6 +51,36 @@ export default function DetailNFT() {
         description: meta.data.description,
         name: meta.data.name,
         tokenURI
+      }
+      return item
+    }))
+    setNfts(items)
+    setLoadingState('loaded') 
+  }
+  */
+  async function loadNFTs() {
+    /* create a generic provider and query for unsold market items */
+    const provider = new ethers.providers.JsonRpcProvider()
+    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, provider)
+    const data = await contract.fetchMarketItems()
+
+    /*
+    *  map over items returned from smart contract and format 
+    *  them as well as fetch their token metadata
+    */
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await contract.tokenURI(i.tokenId)
+      const meta = await axios.get(tokenUri)
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+        tokenURI : tokenUri
       }
       return item
     }))
